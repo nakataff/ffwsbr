2322ee2e65369698f73ac1e9050acac0cceaef96
nakataff <NAKATAFFB4@GMAIL.COM>
2026-08-09 21:50:39 -0300

    Add files via upload

--- ffws-br-2026-s2/config.js
+++ ffws-br-2026-s2/config.js
@@ -1,7 +1,7 @@
 window.FFWS_BR_2026_S2_CONFIG = Object.freeze({
   teamsUrl: 'ffws-br-2026-s2/teams.json?v=20260728-calendar-v1',
-  stagesUrl: 'ffws-br-2026-s2/stages.json?v=20260808-s2-dia3-v1',
-  playersUrl: 'ffws-br-2026-s2/players.json?v=20260808-s2-dia3-v1',
+  stagesUrl: 'ffws-br-2026-s2/stages.json?v=20260809-s2-dia4-v1',
+  playersUrl: 'ffws-br-2026-s2/players.json?v=20260809-s2-dia4-v1',
   datesUrl: 'ffws-br-2026-s2/dates.json?v=20260728-calendar-v1',
   layout: {
     classificatoria: {
