1d7462afc51ffdd911923b0e980e16b4d1ab9305
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-09 13:56:13 -0300

    Adiciona logos da planilha e dados opcionais de títulos

--- js/admin.js
+++ js/admin.js
@@ -1,2 +1,3 @@
 import './admin-core.js?v=20260909-admin-sections-titles-v1';
 import './admin-sections-titles.js?v=20260909-admin-sections-titles-v1';
+import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
