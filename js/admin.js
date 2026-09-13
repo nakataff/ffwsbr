e1e7b6f61f6215a24ed8be014382d4c0ad9f6a6e
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-13 18:56:52 -0300

    Inclui atalho para dados ao vivo

--- js/admin.js
+++ js/admin.js
@@ -2,3 +2,4 @@ import './admin-core.js?v=20260909-admin-sections-titles-v1';
 import './admin-sections-titles.js?v=20260909-admin-sections-titles-v1';
 import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
 import './admin-editor-link.js?v=20260913-photo-editor-v1';
+import './admin-live-data-link.js?v=20260913-live-data-v1';
