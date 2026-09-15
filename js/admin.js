6721fb488be47429c5c4bae800e3247f3a603ab4
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-15 15:04:16 -0300

    Add persistent live standings control to admin

--- js/admin.js
+++ js/admin.js
@@ -4,3 +4,4 @@ import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
 import './admin-editor-link.js?v=20260913-central-tools-v2';
 import './admin-live-data-link.js?v=20260915-community-rewards-v1';
 import './admin-live-standings-v3.js?v=20260915-live-standings-v3';
+import './admin-live-standings-persistence.js?v=20260915-live-standings-persist-v1';
