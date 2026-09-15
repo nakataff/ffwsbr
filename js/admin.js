f2e8419fd057ce25282820b00e1d7f1f5b2687e9
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-15 14:47:44 -0300

    Re-enable live standings with fixed admin module

--- js/admin.js
+++ js/admin.js
@@ -3,3 +3,4 @@ import './admin-sections-titles.js?v=20260909-admin-sections-titles-v1';
 import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
 import './admin-editor-link.js?v=20260913-central-tools-v2';
 import './admin-live-data-link.js?v=20260915-community-rewards-v1';
+import './admin-live-standings-v2.js?v=20260915-live-standings-v2';
