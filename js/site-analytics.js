c1907b0ec900c136cdd5895dbc75f007fcd7bf1f
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-21 15:02:15 -0300

    Atualiza fallback do check-in sem polling

--- js/site-analytics.js
+++ js/site-analytics.js
@@ -27,7 +27,7 @@
 
   if (/\/interacoes\.html$/i.test(location.pathname) && !document.querySelector('script[data-cff-community-checkin]')) {
     const script = document.createElement('script');
-    script.src = 'js/community-checkin.js?v=20260921-community-checkin-v4';
+    script.src = 'js/community-checkin.js?v=20260921-no-poll-v5';
     script.async = false;
     script.dataset.cffCommunityCheckin = '1';
     document.head.appendChild(script);
