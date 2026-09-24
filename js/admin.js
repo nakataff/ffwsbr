c4165f474d03857136e4b1bfb48b64cb023562ad
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-24 13:19:15 -0300

    Atualiza cache final dos módulos do admin

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
-import './admin-core.js?v=20260924-live-filters-v3';
-import './admin-sections-titles.js?v=20260924-tools-tab-v2';
+import './admin-core.js?v=20260924-live-filters-v4';
+import './admin-sections-titles.js?v=20260924-tools-tab-v3';
 import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
 import './admin-editor-link.js?v=20260913-central-tools-v2';
 import './admin-live-data-link.js?v=20260915-community-rewards-v1';
