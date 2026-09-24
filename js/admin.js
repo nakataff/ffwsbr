c9c05490f2f5a7250b034be055ee4abab8f7853f
nakataff <NAKATAFFB4@GMAIL.COM>
2026-09-24 13:40:45 -0300

    Atualiza módulos do painel após reorganizar ferramentas

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
-import './admin-core.js?v=20260924-live-filters-v4';
-import './admin-sections-titles.js?v=20260924-tools-tab-v3';
+import './admin-core.js?v=20260924-detective-tools-v5';
+import './admin-sections-titles.js?v=20260924-tools-above-tabs-v4';
 import './admin-titles-extras.js?v=20260909-admin-title-extras-v1';
 import './admin-editor-link.js?v=20260913-central-tools-v2';
 import './admin-live-data-link.js?v=20260924-private-tools-v2';
